a8a27b67b7b7387649b010aef7f0404886d4b4f7
Pavel Quevedo <pavelquevedo@outlook.com>
2021-09-19 11:03:46 -0600

    Add a common library to be used between projects.

---
+++ WebChat.Utils/Common/Models/Request/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebChat.Utils.Common.Models.Request
+{
+    public class LoginRequest
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}

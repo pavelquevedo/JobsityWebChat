4de135eafbd1742512b88a039f43cb8360164678
Pavel Quevedo <pavelquevedo@outlook.com>
2021-09-19 15:13:57 -0600

    Add message commons to Utils.

---
+++ WebChat.Utils/Common/Models/Response/MessageResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebChat.Utils.Common.Enum;
+
+namespace WebChat.Utils.Common.Models.Response
+{
+    public class MessageResponse
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public int UserID { get; set; }
+        public string UserFullName { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public MessageType MessageType { get; set; } 
+    }
+}

990a3a9af34d77838d4c045a2ca11bf9059ce771
Pavel Quevedo <pavelquevedo@outlook.com>
2021-09-19 11:06:33 -0600

    Add BaseController, AccountController which provides user authentication and JWT handlers.

--- JobsityWebChat/App_Start/WebApiConfig.cs
+++ JobsityWebChat/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JobsityWebChat.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,6 +14,9 @@ namespace JobsityWebChat
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            //Adding token validation handler
+            config.MessageHandlers.Add(new TokenValidationHandler());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
---
+++ JobsityWebChat/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using JobsityWebChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobsityWebChat.Controllers
+{
+    public class BaseController : ApiController
+    {
+        private WebChatDBEntities _db;
+
+        public BaseController()
+        {
+            this._db = new WebChatDBEntities();
+        }
+
+        public WebChatDBEntities dbContext
+        {
+            get
+            {
+                return this._db;
+            }
+        }
+    }
+}

2b941aa914b15d840fd2cc38f38d344621cbc215
Pavel Quevedo <pavelquevedo@outlook.com>
2021-09-19 18:32:40 -0600

    -Add WebChat.Client and WebChat.Tests projects. -Add HomeController with Login and Register functionalities. -Add SignalR libraries. -Add Owin Startup class to map SignalR.

---
+++ WebChat.Client/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System.Web.Mvc;
+using WebChat.Client.Models.ViewModels;
+using WebChat.Utils.Common.Models.Request;
+using WebChat.Utils.Common.Models.Response;
+using WebChat.Utils.Tools;
+
+namespace WebChat.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            LoginRequest model = new LoginRequest();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            model.Password = Encrypt.GetSHA256(model.Password).ToUpper();
+
+            UserResponse userResponse =
+                RequestUtil.ExecuteWebMethod<UserResponse>("api/authentication/authenticate", RestSharp.Method.POST, string.Empty, model);
+
+            if (userResponse != null)
+            {
+                Session["User"] = userResponse;
+                return RedirectToAction("Index", "Lobby");
+            }
+
+            ViewBag.error = "Incorrect credentials";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UserResponse userResponse =
+            RequestUtil.ExecuteWebMethod<UserResponse>("api/user/register", RestSharp.Method.POST, string.Empty, model);
+
+            if (userResponse != null)
+            {
+                Session["User"] = userResponse;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebChat.Client/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebChat.Client.Startup))]
+
+namespace WebChat.Client
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}

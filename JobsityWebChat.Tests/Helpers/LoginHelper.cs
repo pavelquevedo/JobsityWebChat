5547aff2f53ecf07f878a4d9fcc37ab4321b542e
Pavel Quevedo <pavelquevedo@outlook.com>
2021-09-22 15:58:32 -0600

    -TestMethods added to AccountControllerTest and UserControllerTest. -RandomNameGenerator helper added.

---
+++ JobsityWebChat.Tests/Helpers/LoginHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebChat.Api.Controllers;
+using WebChat.Utils.Common.Models.Request;
+using WebChat.Utils.Common.Models.Response;
+
+namespace JobsityWebChat.Tests.Helpers
+{
+    public class LoginHelper
+    {
+
+
+        public static string GetUserToken()
+        {
+            LoginRequest loginRequest = new LoginRequest()
+            {
+                Login = "pavel",
+                Password = "03AC674216F3E15C761EE1A5E255F067953623C8B388B4459E13F978D7C846F4"
+            };
+
+            AccountController accountController = new AccountController();
+            Task<IHttpActionResult> authTask = Task.Run(() => accountController.Authenticate(loginRequest));
+            authTask.Wait();
+
+            var contentResult = authTask.Result as OkNegotiatedContentResult<UserResponse>;
+            if (authTask.Result != null)
+            {
+                return contentResult.Content.AccessToken;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ JobsityWebChat.Tests/Helpers/RandomNameGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsityWebChat.Tests.Helpers
+{
+    public class RandomNameGenerator
+    {
+        public static string Generate(int len)
+        {
+            Random r = new Random();
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+            string Name = "";
+            Name += consonants[r.Next(consonants.Length)].ToUpper();
+            Name += vowels[r.Next(vowels.Length)];
+            int b = 2;
+            while (b < len)
+            {
+                Name += consonants[r.Next(consonants.Length)];
+                b++;
+                Name += vowels[r.Next(vowels.Length)];
+                b++;
+            }
+
+            return Name;
+
+
+        }
+    }
+}
